7758b3ec617fbf798f3da47bebef9ea979d9fc7b
Vuong Tran <vuongtranitc@gmail.com>
2022-10-04 18:56:21 +0700

    Initial commit

---
+++ bin/dart_fundamental29092022.dart
@@ -0,0 +1,24 @@
+import 'package:dart_fundamental29092022/dart_fundamental29092022.dart' as dart_fundamental29092022;
+
+void main(List<String> arguments) {
+  //print('Hello world: ${dart_fundamental29092022.calculate()}!');
+
+  //1: kieu du lieu
+  // kieu so
+  //num weight=75.5;
+  // kieu chuoi
+  //String name ="Vuong";
+  // kieu boolen
+  //bool isMale = true;
+  // Kieu run
+  //String unicode1 = "♑";
+  //Runes runes = Runes(
+  //  '\u{2651} \u{2652} \u{2653}'
+  //);
+  //print(String.fromCharCodes(runes));
+  //print(unicode1);
+
+  var a=10;
+  print(a);
+
+}
---
+++ test/dart_fundamental29092022_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_fundamental29092022/dart_fundamental29092022.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
